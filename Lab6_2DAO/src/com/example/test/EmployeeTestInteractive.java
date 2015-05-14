6bf292472dfcab9d440a8b58ebcf859ba3239a64
A00178169 <A00178169@W301-A31.studentnet.ait.ie>
2015-05-14 13:11:34 +0100

    Oops

--- Lab6_2DAO/src/com/example/test/EmployeeTestInteractive.java
+++ Lab6_2DAO/src/com/example/test/EmployeeTestInteractive.java
@@ -31,6 +31,7 @@ public class EmployeeTestInteractive {
         Employee emp;
         String action;
         int id;
+        System.out.println("Please choose from one of the following options");
         System.out.println("\n\n[C]reate | [R]ead | [U]pdate | [D]elete | [L]ist | [Q]uit: ");
         action = in.readLine();
         if ((action.length() == 0) || action.toUpperCase().charAt(0) == 'Q') {
